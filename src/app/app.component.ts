c92f83553e67b09d911d6c2dbaf02cf2dbcb28c2
Jesus <jmorenojimenez@safareyes.es>
2024-10-02 11:37:45 +0200

    todo2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+    imports: [IonApp, IonRouterOutlet, NavbarComponent],
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {IonicModule} from "@ionic/angular";
-import {GrupoComponent} from "../grupo/grupo.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NavbarComponent, IonicModule, GrupoComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NavbarComponent, IonicModule],
 })
 export class HomePage {
   constructor() {}
